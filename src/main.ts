d6aa917ba49166a3399d7a35cc8970f88c93af08
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-07-09 00:23:58 +0430

    change sunchronize

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: true,
-  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  //   allowedHeaders: 'Content-Type, Accept, Authorization',
-  // });
-  // app.use(helmet());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT || 5000;
   await app.listen(PORT, () => {
