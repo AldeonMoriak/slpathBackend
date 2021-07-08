6dbc5cf55cb5510e6c42207dceb8523e997cdfbe
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-07-09 00:08:11 +0430

    h

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
-  app.use(helmet());
+  // app.enableCors({
+  //   origin: true,
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   allowedHeaders: 'Content-Type, Accept, Authorization',
+  // });
+  // app.use(helmet());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT || 5000;
   await app.listen(PORT, () => {
